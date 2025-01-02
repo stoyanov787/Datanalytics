be74e140d546f901edecf7648466188790238804
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2025-01-02 20:25:59 +0200

    Update urls.py

--- datanalytics/projects/urls.py
+++ datanalytics/projects/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.param),
+    path('', views.projects),
+    path('download_csv/', views.download_csv),
+    path('project_creation/', views.project_creation),
+    path('param/', views.param),
     path('observation_date_column_choice/', views.observation_date_column_choice, name='observation_date_column_choice')
 ]
