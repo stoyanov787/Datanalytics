10a63dcc7d7de445294dfbbeed3ee48bc4623a60
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2025-01-02 20:24:49 +0200

    Update views.py

--- datanalytics/projects/views.py
+++ datanalytics/projects/views.py
@@ -1,17 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .forms import ParamForm
+from .forms import ParamForm, ProjectForm
+from .models import Project
+import pandas as pd
 
 def param(request):
     if request.user.is_authenticated:
         context = {}
         context['form'] = ParamForm(request=request)
-        return render(request, 'param.html', context)
+        return render(request, 'param/param.html', context)
     else:
         return HttpResponse("You are not logged in")
     
+
+# make this class with form View(form view to Use model Project)
 def observation_date_column_choice(request):
     form = ParamForm(request=request)
     return HttpResponse(form["t1df"])
 
+def project_creation(request):
+    if request.user.is_authenticated:
+        context = {}
+        context['form'] = ProjectForm()
+        return render(request, 'project_creation.html', context)
+    else:
+        return HttpResponse("You are not logged in")
+    
+def projects(request):
+    if not request.user.is_authenticated:
+        return redirect('/accounts/login')
+    
+    project_name = request.GET.get('project_name')
+    if not project_name:
+        user_projects = Project.objects.filter(user=request.user)
+        context = {
+            'projects': user_projects
+        }
+        return render(request, 'projects/all_projects.html', context)
+
+    project = get_object_or_404(Project, user=request.user, name=project_name)
+
+    context = {
+            'project': project
+    }
+    return render(request, 'projects/project.html', context)
+
+def download_csv(request):
+    project_name = request.GET.get('project_name')
+
+    if not project_name:
+        redirect('/')
+
+    project = get_object_or_404(Project, user=request.user, name=project_name)
+    df = pd.read_csv(project.input_dataframe)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="input.csv"'
+    df.to_csv(response, index=False)
+    
+    return response
+
+
+    
+
+    
 
