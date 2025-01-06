266ddca629853c25e8e2adf0185338f8535784df
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2025-01-06 17:27:41 +0200

    Update urls.py

--- datanalytics/projects/urls.py
+++ datanalytics/projects/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.projects),
+    path('prep/', views.prep),
     path('download_csv/', views.download_csv),
     path('project_creation/', views.project_creation),
     path('param/', views.param),
