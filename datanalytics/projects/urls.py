ec167e3ea188066324132b9d1cb15056ae4d61a6
stoyanov787 <98825069+stoyanov787@users.noreply.github.com>
2025-01-13 16:36:49 +0200

    Update urls.py

--- datanalytics/projects/urls.py
+++ datanalytics/projects/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.projects),
     path('prep/', views.prep),
+    path('trainandeval/', views.train_and_eval),
     path('download_csv/', views.download_csv),
     path('project_creation/', views.project_creation),
     path('param/', views.param),
-    path('param/observation_date_column_choice/', views.observation_date_column_choice, name='observation_date_column_choice')
+    path('param/observation_date_column_choice/', views.observation_date_column_choice, name='observation_date_column_choice'),
+    path('task-status/<str:task_id>/', views.task_status, name='task_status')
 ]
